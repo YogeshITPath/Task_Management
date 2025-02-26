6693b0d5e975aa5ee3411d703e3c95c69624db6b
Yogesh <yogeshh.itpath@gmail.com>
2025-02-26 15:02:38 +0530

    Created API application using Rest framework and postman

--- api/serializers.py
+++ api/serializers.py
@@ -4,4 +4,4 @@ from tasks.models import Task
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = ['id', 'title', 'description', 'status', 'created_at', 'updated_at']
+        fields = ['id', 'title', 'description', 'status', 'is_completed', 'created_at', 'updated_at']
